3524df880b6dde5313fd1f328856bc73d62ab0ab
Stephen Reindl <stephen.reindl@outlook.de>
2022-05-26 20:48:58 +0200

    feat: implemented execution logic

--- src/task.ts
+++ src/task.ts
@@ -1,14 +1,26 @@
+import {Octokit} from '@octokit/rest'
 import Release from './release'
 
 export default abstract class Task {
   readonly line: string
   releases: Release[]
+  private dryRun = true
+  protected octokit?: Octokit = undefined
 
   constructor(line: string, releases: Release[]) {
     this.line = line
     this.releases = releases
   }
 
-  abstract execute(): boolean
+  abstract execute(): Promise<boolean>
   abstract parse(what: string): boolean
+  setDryRun(dryRun: boolean): void {
+    this.dryRun = dryRun
+  }
+  isDryRun(): boolean {
+    return this.dryRun || !this.octokit
+  }
+  setOctokit(kit: Octokit): void {
+    this.octokit = kit
+  }
 }
